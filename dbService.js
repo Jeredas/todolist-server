6793d673a60b9bf665f5b0ab3fd01cf95261c01a
Jeredas <efioss93@mail.ru>
2021-06-19 14:18:03 +0300

    fix : add bd
    bd name ChessCodeJam

--- dbService.js
+++ dbService.js
@@ -13,7 +13,7 @@ class DatabaseService{
     });
 
     mongo.connect().then(()=>{
-      this.db = mongo.db('zhanzs');
+      this.db = mongo.db('ChessCodeJam');
       console.log('connected');
     });
   }
